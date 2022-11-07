1113febef5023b7745b8c51bacaedb70dcf56f43
Rafael Silva <deussilvao@yahoo.com>
2022-11-07 10:43:54 -0300

    projeto-final
    etapa final do projeto de gerador de senha em C#

---
+++ GeradorDeSenha/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Text;
+
+namespace GeradorDeSenha
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite o número de caracteres que terá sua senha: ");
+            int qtd = int.Parse(Console.ReadLine());
+
+            while (qtd > 0)
+            {
+                string caracteresPossiveis =
+                    "ABCDEFGHIJKLMNOPQRSTUVWXYZ" +
+                    "abcdefghijklmnopqrstuvwxyz" +
+                    "0123456789" +
+                    "!@#$%&";
+
+                Random sorteio = new Random();
+                int numeroSorteado = 0;
+                StringBuilder senha = new StringBuilder();
+
+                for (int i = 0; i < qtd; i++)
+                {
+                    numeroSorteado = sorteio.Next(0, caracteresPossiveis.Length - 1);
+                    senha.Append(caracteresPossiveis[numeroSorteado]);
+                }
+                Console.WriteLine();
+                Console.WriteLine(senha.ToString());
+                Console.WriteLine();
+                Console.Write("Digite o número de caracteres que terá sua senha: ");
+                qtd = int.Parse(Console.ReadLine());
+            }
+        }
+    }
+}
